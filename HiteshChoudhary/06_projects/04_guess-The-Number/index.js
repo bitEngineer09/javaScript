ae964f2479f46af3c4db7120c88e2bd773de2423
bitEngineer09 <gautam.columbian@gmail.com>
2025-03-17 12:35:16 +0530

    17 Mar

---
+++ HiteshChoudhary/06_projects/04_guess-The-Number/index.js
@@ -0,0 +1,41 @@
+// const form = document.querySelector('form')
+const userInput = parseInt(document.querySelector('#guessField').value)
+const sbmt = document.querySelector('#subt')
+const prevGuessSlot = document.querySelector('#guesses')
+const remGuessSlot = document.querySelector('.lastResult')
+const lowHigh = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas')
+
+const random = Math.round(Math.random()*100+1)
+console.log(random);
+
+let playGame = true
+let prevGuess = []
+let numOfGuess = 1
+
+
+
+function validateGuess(guess) {
+//
+}
+
+function checkGuess(guess) {
+
+}
+
+function displayGuess(guess) {
+
+}
+
+function displayMessage(message) {
+
+}
+
+function endGame() {
+
+}
+
+function enndGame() {
+
+}
+
